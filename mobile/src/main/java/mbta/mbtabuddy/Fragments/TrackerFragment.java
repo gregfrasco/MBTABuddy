2a286970dbb5374e8124f66dd30b6937f6c5ace1
Greg Frasco <frascog@wit.edu>
2016-08-04 11:06:19 -0400

    Added Map Fragment to Traker

--- mobile/src/main/java/mbta/mbtabuddy/Fragments/TrackerFragment.java
+++ mobile/src/main/java/mbta/mbtabuddy/Fragments/TrackerFragment.java
@@ -2,25 +2,40 @@ package mbta.mbtabuddy.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import mbta.mbtabuddy.R;
 
+public class TrackerFragment extends Fragment implements OnMapReadyCallback {
 
-public class TrackerFragment extends Fragment {
-
-    public TrackerFragment() {
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstancetate){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_tracker, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MapFragment fragment = (MapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng marker = new LatLng(42.3132883,-71.1972408);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, 13));
+        googleMap.addMarker(new MarkerOptions().title("Hello Google Maps!").position(marker));
     }
 }
